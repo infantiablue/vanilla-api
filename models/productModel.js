b8ad1ee83d8e1fef4fc1b922dd3fae726bce74e2
infantiablue <dangtruong@gmail.com>
2020-10-18 19:35:20 +0700

    initialize API server

---
+++ models/productModel.js
@@ -0,0 +1,3 @@
+const products = require("../data/products.json");
+
+function find() {}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const products = require("./data/products.json");
+
 const server = http.createServer((req, res) => {
 	if (req.url == "/api/products" && req.method == "GET") {
 		res.writeHead(200, { "Content-Type": "application/json" });
